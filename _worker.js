963688f471a7722ce9f5c8cfb544f146d42a0139
Kannavi Mishra <79000269+cloudcreatr@users.noreply.github.com>
2023-12-25 12:57:21 +0000

    Remove unnecessary configuration files and code

---
+++ _worker.js
@@ -0,0 +1,24 @@
+import { performance } from 'perf_hooks';
+
+
+export default {
+    async fetch(request, env, ctx) {
+        let url = new URL(request.url);
+        
+        if (url.searchParams.has("set")) {
+            await env.kv.put("om", url.searchParams.get("set"));
+
+            const start = performance.now();
+            const value = await env.kv.get("om", { cacheTtl: 60 });
+            const stop = performance.now();
+            console.log("same data center timing", stop - start);
+            return new Response(`updated hello ${value}`);
+        }
+        
+        const start = performance.now();
+        const value = await env.kv.get("om", { cacheTtl: 60 });
+        const stop = performance.now();
+        console.log("cache around the world timing", stop - start);
+        return new Response(`hello ${value}`);
+    }
+};
